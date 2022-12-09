96a0907ca029c946b8bc47d95326674606307af9
Rahul <rahul10kr7@gmail.com>
2022-12-09 18:08:17 +0530

    app start

---
+++ react_signup/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
