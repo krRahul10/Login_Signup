b349eca50d021e644b2ba56fa72d9585d38540c8
Rahul <rahul10kr7@gmail.com>
2022-12-09 19:24:00 +0530

    sign input done

--- react_signup/src/Components/Home.jsx
+++ react_signup/src/Components/Home.jsx
@@ -2,8 +2,51 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Sign_img from "./Sign_img";
+import { useState } from "react";
 
 const Home = () => {
+  const [inputVal, setInputVal] = useState({
+    name: "",
+    email: "",
+    date: "",
+    password: "",
+  });
+
+  //   console.log(inputVal);
+
+  const getData = (e) => {
+    const { value, name } = e.target;
+    setInputVal(() => {
+      return {
+        ...inputVal,
+        [name]: value,
+      };
+    });
+  };
+
+  const addData = (e) => {
+    e.preventDefault();
+    
+
+    const { name, email, date, password } = inputVal;
+
+    if (name === "") {
+      alert("name field is required");
+    } else if (email === "") {
+      alert("email field is required");
+    } else if (!email.includes("@")) {
+      alert("please enter valid email");
+    } else if (date === "") {
+      alert("date field is required");
+    } else if (password === "") {
+      alert("password field is required");
+    } else if (password.length < 5) {
+      alert("password must be greater than 5");
+    } else {
+        console.log("data added successfully")
+        alert("done")
+    }
+  };
   return (
     <div>
       Home
@@ -13,20 +56,40 @@ const Home = () => {
             <h3 className="text-center col-lg-6">Sign up</h3>
             <Form>
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Control type="text" placeholder="Enter Your Name" />
+                <Form.Control
+                  type="text"
+                  onChange={getData}
+                  name="name"
+                  placeholder="Enter Your Name"
+                />
               </Form.Group>
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control
+                  type="email"
+                  onChange={getData}
+                  name="email"
+                  placeholder="Enter email"
+                />
               </Form.Group>
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Control type="date" placeholder="Enter email" />
+                <Form.Control
+                  type="date"
+                  onChange={getData}
+                  name="date"
+                  placeholder="Enter email"
+                />
               </Form.Group>
 
               <Form.Group
                 className="mb-3 col-lg-6"
                 controlId="formBasicPassword"
               >
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  type="password"
+                  onChange={getData}
+                  name="password"
+                  placeholder="Password"
+                />
               </Form.Group>
 
               <Button
@@ -37,6 +100,7 @@ const Home = () => {
                   marginLeft: "-50%",
                   backgroundColor: "rgb(67,185,127)",
                 }}
+                onClick={addData}
               >
                 Submit
               </Button>
@@ -50,7 +114,7 @@ const Home = () => {
               Already Have an Account <span>SignIn</span>
             </p>
           </div>
-          <Sign_img/>
+          <Sign_img />
         </section>
       </div>
     </div>
